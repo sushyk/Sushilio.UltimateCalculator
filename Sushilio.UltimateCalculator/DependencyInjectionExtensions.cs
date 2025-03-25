89ae576b1234ae1174117b0e258f8d1cb23c4124
Sushil <ksk.sushil.kumar@gmail.com>
2025-03-25 15:42:45 +0530

    expose DI extension method

---
+++ Sushilio.UltimateCalculator/DependencyInjectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Sushilio.UltimateCalculator
+{
+    public static class CalculatorServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSushilioUltimateCalculator(this IServiceCollection services)
+        {
+            services.AddTransient<ICalculator, Calculator>();
+
+            return services;
+        }
+    }
+}
